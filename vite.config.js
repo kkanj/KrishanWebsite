7ec541de1d5bcdd26d2ef15bc45439b5baa1ba39
Krishan <krishankanji2003@gmail.com>
2024-07-04 05:28:04 -0700

    index.html ./dist and viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
-
+import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svgr(),
     react(),
-
   ],
+  base: './',
+  resolve: {
+    alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+    ],
+},
 })
