a7b1c9b979ec5d29f6b0b20287a8c890ee34cb02
Krishan <krishankanji2003@gmail.com>
2024-07-04 17:08:28 -0700

    htaccess added and removed vitepluginssr

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
-import ssr from 'vite-plugin-ssr/plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svgr(),
     react(),
-    ssr(),
   ],
   base: '/',
   
