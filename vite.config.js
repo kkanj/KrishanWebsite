0d1c858437d9e2d7742d6e7494b88ef73a9441f8
Krishan <krishankanji2003@gmail.com>
2024-07-04 17:20:31 -0700

    public htaccess and tab title added

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import copy from "vite-plugin-copy";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svgr(),
         react(),
-        copy({
-            targets: [
-                { src: ".htaccess", dest: "dist" }, // Copies .htaccess to the dist folder
-            ],
-            hook: "writeBundle", 
-        }),
+        
     ],
     base: "/",
 });
