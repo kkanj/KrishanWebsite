da04975cc05b48cae777b258a1ac6c48ce3851e3
Krishan Kanji <krishankanji2003@gmail.com>
2024-08-31 00:02:12 -0700

    Created hash function in order to stop browser cache from prohibiting the latest build content to user

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-
+import { hash } from "./src/utils/functions.js";
 export default defineConfig({
-    plugins: [
-        svgr(),
-        react(),
-    ],
+    plugins: [svgr(), react()],
     base: "/",
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name]` + hash + `.js`,
+                chunkFileNames: `[name]` + hash + `.js`,
+                assetFileNames: `[name]` + hash + `.[ext]`,
+            },
+        },
+    },
 });
