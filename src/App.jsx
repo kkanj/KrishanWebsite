d8600a803a7aef8292d3d8e9652279101e0c97dd
Krishan Kanji <krishankanji2003@gmail.com>
2024-09-12 22:48:52 -0700

    reload issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { react } from "react";
+import { HashRouter } from "react-router-dom";
 import "./App.css";
 import {
     Splash,
@@ -23,16 +24,17 @@ function App() {
     const location = useLocation();
     return (
         <div className="bg-background h-screen w-screen">
-            <AnimatePresence mode="wait" initial={location.pathname == '/'}>
+            <AnimatePresence mode="wait" initial={location.pathname == "/"}>
                 
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/about" element={<AboutMe />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/project0" element={<Project0 />} />
-                    <Route path="/placeholder" element={<Placeholder />} />
-                </Routes>
+                    <Routes location={location} key={location.pathname}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/about" element={<AboutMe />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/project0" element={<Project0 />} />
+                        <Route path="/placeholder" element={<Placeholder />} />
+                    </Routes>
+                
             </AnimatePresence>
         </div>
     );
