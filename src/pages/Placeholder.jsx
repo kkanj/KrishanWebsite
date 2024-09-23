8784ad998d7f000ee866cab0891bb5e035614ff7
Krishan Kanji <krishankanji2003@gmail.com>
2024-09-22 18:51:11 -0700

    update message for unfinished pages

--- src/pages/Placeholder.jsx
+++ src/pages/Placeholder.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Placeholder = () => {
   return (
-    <div>SOMETHING WILL BE HERE</div>
+    <div>This page is currently being updated to my new design!</div>
   )
   
 }
