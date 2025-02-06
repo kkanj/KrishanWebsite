166030c4196eb370e6c5d53f3dbb69551dcc0702
Krishan Kanji <krishankanji2003@gmail.com>
2025-02-05 18:27:58 -0800

    added dna sequencing position

--- src/components/experienceData.js
+++ src/components/experienceData.js
@@ -1,5 +1,17 @@
 import around from "../assets/images/around.png";
+
+import dna from "../assets/images/dna.png";
 const experience = [
+    {
+        image: dna,
+        title: "UC Berkeley DNA Sequencing Facility",
+        language: "Software & Data Systems Developer",
+        description:
+            "Developing a web-based sample submission and data retrieval portal for DNA sequencing customers. Leveraging the Berkeley Savio Supercomputer to process large-scale DNA sequencing data, enabling high-speed sequence analysis and automated customer data delivery. Working with Python, SQL, cloud computing, and bioinformatics tools to optimize sequencing workflows and improve accessibility for researchers.",
+        link: "https://dnaseq.berkeley.edu/",
+        className:
+            "md:col-span-6 md:row-span-1 lg:col-span-4 lg:row-span-1 2xl:col-span-3 2xl:row-span-1 ",
+    },
     {
         image: around,
         title: "A Round Entertainment",
